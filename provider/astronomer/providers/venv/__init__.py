ee889b6c2475148284578deeaa242b629516ff03
Ash Berlin-Taylor <ash_github@firemirror.com>
2023-03-10 18:02:38 +0000

    Rename to astro-provider-venv

--- provider/astronomer/providers/venv/__init__.py
+++ provider/astronomer/providers/venv/__init__.py
@@ -3,8 +3,8 @@ __version__ = "1.0.0a2"
 
 def get_provider_info():
     return {
-        "package-name": "astro-providers-venv",
-        "name": "astro-providers-venv",
+        "package-name": "astro-provider-venv",
+        "name": "astro-provider-venv",
         "description": "Easily create and use Python Virtualenvs in Apache Airflow",
         "task-decorators": [
             {
